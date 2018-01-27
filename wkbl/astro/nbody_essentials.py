05960f44af1a209d5d202461be31336562cddc5e
NUNEZ Arturo <arturo.nunez@lam.fr>
2018-01-27 11:46:48 +0100

    nbe for new fams2

--- wkbl/astro/nbody_essentials.py
+++ wkbl/astro/nbody_essentials.py
@@ -204,7 +204,7 @@ def check_particles(path):
             if linum==7:
                 n_dm = float(row[-1])
             if linum==8:
-                n_t = float(row[-1])
+                n_st = float(row[-1])
         n_tot = n_st + n_dm
         print n_tot, n_dm, n_st
         return int(n_tot), int(n_dm), int(n_st)

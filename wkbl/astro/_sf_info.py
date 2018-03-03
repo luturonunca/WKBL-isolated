bceb898ac119cdefee368273a606232e604b63e2
NUNEZ Arturo <arturo.nunez@lam.fr>
2018-03-03 16:30:10 +0100

    Automatic commit samedi 3 mars 2018, 16:30:01 (UTC+0100)

--- wkbl/astro/_sf_info.py
+++ wkbl/astro/_sf_info.py
@@ -21,18 +21,19 @@ class SF_info:
         comov= kwargs.get('comov',False)
         self._center_history = np.array([[0,0,0]])
         self.data = nbe._get_center(file_path, sf_hist=True)
+        stars = np.where(self.data[:,0]==0)[0]
         if (comov):
-            self.pos3d = self.data[:,4:7] * p.simutokpc / p.aexp
+            self.pos3d = self.data[stars,4:7] * p.simutokpc / p.aexp
         else:
-            self.pos3d = self.data[:,4:7] * p.simutokpc
-        self.mass = self.data[:,3] * p.simutoMsun
-        self.id = self.data[:,1]
-        self.hsml = 25000./(2.**(self.data[:,2]))
-        self.rho = self.data[:,10]*self.p.simutoMsun*(self.p.simutokpc)**-3
-        self.temp2 = self.data[:,15]* self.p.scale_T2
-        self.met = self.data[:,16] 
-        self.sigma2 = self.data[:,17] * (self.p.simutokms**2)
-        cs2 = 0.666*self.data[:,15] * (self.p.simutokms**2)
+            self.pos3d = self.data[stars,4:7] * p.simutokpc
+        self.mass = self.data[stars,3] * p.simutoMsun
+        self.id = self.data[stars,1]
+        self.hsml = 25000./(2.**(self.data[stars,2]))
+        self.rho = self.data[stars,10]*self.p.simutoMsun*(self.p.simutokpc)**-3
+        self.temp2 = self.data[stars,15]* self.p.scale_T2
+        self.met = self.data[stars,16] 
+        self.sigma2 = self.data[stars,17] * (self.p.simutokms**2)
+        cs2 = 0.666*self.data[stars,15] * (self.p.simutokms**2)
         cs2[np.where(cs2<=0)] = 0.002
         self.cs2 = cs2
         self.M2 = self.sigma2 / self.cs2

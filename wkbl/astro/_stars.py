d9db4625a19e7ee0e12e49b738a74e8d1c557d82
NUNEZ Arturo <arturo.nunez@lam.fr>
2018-05-24 07:26:05 +0200

    debug2

--- wkbl/astro/_stars.py
+++ wkbl/astro/_stars.py
@@ -56,7 +56,7 @@ class _stars:
         self.R = np.sqrt((self.pos3d[:,0]**2)+(self.pos3d[:,1]**2))
         self.r = np.sqrt((self.pos3d[:,0]**2)+(self.pos3d[:,1]**2)+(self.pos3d[:,2]**2))
         self.phi = np.arctan2(np.copy(self.pos3d[:,1]),np.copy(self.pos3d[:,0]))
--        self.theta = np.arccos(np.copy(self.pos3d[:,0]),np.copy(self.r))
+-       self.theta = np.arccos(np.copy(self.pos3d[:,0]),np.copy(self.r))
         ## velocities ###
         vx,vy,vz = self.vel3d[:,0],self.vel3d[:,1],self.vel3d[:,2]
         self.v = np.sqrt((vx**2) + (vy**2) + (vz**2))
